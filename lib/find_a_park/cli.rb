0fa6a556053e4597165203ae9934bfb434ff8b5d
Nick Romans <nicholas.romans@gmail.com>
2018-10-23 20:03:04 +0000

    added very basic CLI code

--- lib/find_a_park/cli.rb
+++ lib/find_a_park/cli.rb
@@ -4,17 +4,37 @@ class FindAPark::CLI
 
   def call
     puts "Welcome to Park Finder!"
-    lists_state
+    lists_states
     user_selection
+    bye
   end
 
-  def lists_state
+  def lists_states
     puts "We will show parks by state."
     puts "States go here"
   end
 
   def user_selection
-    puts "Please choose the state that interests you."
+    input = nil
+    while input != "exit"
+      puts "Please enter the number of the state to get more info, 'states' to see the list again or type 'exit' to quit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Parks within that particular state"
+      when "2"
+        puts "Parks within that other particular state"
+      when "states"
+        lists_states
+      else
+        puts "Didn't quite unstand that"
+      end
+    end
+  end
+
+  def bye
+    puts "Please visit us again to learn more about your national parks"
+    puts "Goodbye"
   end
 
 end
