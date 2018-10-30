6b57490e1b631ac3bbcd6c5505f7d6e243807d4a
Nick Romans <nicholas.romans@gmail.com>
2018-10-30 01:55:47 +0000

    successfully listed scraped state data with numbers

--- lib/find_a_park/scraper.rb
+++ lib/find_a_park/scraper.rb
@@ -1,25 +1,20 @@
 
 class FindAPark::Scraper
 
+
+
   def self.whole_page
     Nokogiri::HTML(open("https://www.nps.gov/findapark/index.htm"))
   end
 
   def self.states_for_days
-    @states = self.whole_page.css('form#simpleStateForm').text
-    @states_array = @states.split("\n")
-    @states_array.slice!(0..3)
-    @states_array
+    @states = self.whole_page.css('form#simpleStateForm').text #scrapes from page to specific form
+    @states_array = @states.split("\n") #separates states and puts into array
+    @states_array.slice!(0..3) #removes unwanted data
+    @states_array.each_with_index do |state, idx| #iterates and puts out states with numbers
+      puts "#{idx + 1}." "#{state}"
   end
-
-  def self.list_each_state
-    @states = []
-    self.states_for_days.each do |state|
-      @states << state
-    end
-    @states
-  end
-
+end
 
 end
 
