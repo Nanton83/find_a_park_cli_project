0b92bd5b75de699a8d1db7d779c600404802fa9e
Nick Romans <nicholas.romans@gmail.com>
2018-11-06 19:43:12 +0000

    changed scraped site to new location

--- lib/find_a_park/scraper.rb
+++ lib/find_a_park/scraper.rb
@@ -3,14 +3,14 @@ require 'pry'
 
 class FindAPark::Scraper
 
-
-
   def self.whole_page
-    Nokogiri::HTML(open("https://www.nps.gov/findapark/index.htm"))
+    Nokogiri::HTML(open("https://state.1keydata.com/national-parks-by-state.php"))
   end
 
-  def self.scrape_states  #<div id="nationalMap-map">
-    self.whole_page.css('div#nationalMap-map')
+  def self.scrape_states_and_parks
+    self.whole_page.css('.content3')
+    binding.pry
+
   end
 
   def self.scrape_parks
@@ -19,3 +19,6 @@ class FindAPark::Scraper
 
 
 end
+# page = Nokogiri::HTML(open(PAGE_URL))
+# news_links = page.css("a").select{|link| link['data-category'] == "news"}
+# news_links.each{|link| puts link['href'] }
